90377d2b20cc9d6e7e296a7bdb67c6b9876f0a7f
misrax <fordax05@gmail.com>
2024-06-19 23:07:59 +0300

    test(sqlite): Unit and Integration test cases for the DatabaseInitializaer

--- tests/database/test_database_intitializer.py
+++ tests/database/test_database_intitializer.py
@@ -1,8 +1,43 @@
+import os
 import sqlite3
 import unittest
+from unittest.mock import Mock, patch
 
 from employee_management.core.configurations.configuration import configuration
 from employee_management.database.database_initializer import DatabaseInitializer
+
+
+class DatabaseInitializerUnitTestCase(unittest.TestCase):
+    def setUp(self):
+        os.environ["DATABASE_URL"] = ":memory:"
+        self.config = configuration
+        self.database_initializer = DatabaseInitializer()
+
+    @patch("sqlite3.connect")
+    def test_create_employee_table(self, mock_connect: Mock) -> None:
+        mock_conn = Mock()
+        mock_connect.return_value = mock_conn
+        mock_cursor = Mock()
+        mock_conn.cursor.return_value = mock_cursor
+        db_initializer = DatabaseInitializer()
+        db_initializer.create_employee_table()
+        mock_connect.assert_called_once_with(":memory:")
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once_with(
+            """
+            CREATE TABLE IF NOT EXISTS employee (
+                employee_id TEXT PRIMARY KEY,
+                name TEXT NOT NULL,
+                position TEXT NOT NULL,
+                email TEXT NOT NULL,
+                salary REAL NOT NULL,
+                created_at TEXT NOT NULL,
+                modified_at TEXT NOT NULL
+            )
+        """
+        )
+        mock_conn.commit.assert_called_once()
+        mock_cursor.close.assert_called_once()
 
 
 class DatabaseInitializerIntegrationTestCase(unittest.TestCase):
