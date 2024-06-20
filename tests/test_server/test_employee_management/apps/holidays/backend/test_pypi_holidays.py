180d21cdc6462e35cfdea1ed9400315f49770384
misrax <fordax05@gmail.com>
2024-06-21 01:49:22 +0300

    test(backend): Adding unit test cases for the PyPiHolidaysBackend

--- tests/test_server/test_employee_management/apps/holidays/backend/test_pypi_holidays.py
+++ tests/test_server/test_employee_management/apps/holidays/backend/test_pypi_holidays.py
@@ -1,4 +1,6 @@
 import unittest
+from datetime import date
+from unittest.mock import patch
 
 from faker import Faker
 from holidays import PUBLIC
@@ -21,6 +23,38 @@ class PyPiHolidaysTests(unittest.TestCase):
         )
         for holiday in get_holidays:
             self.assertEqual(holiday[0].year, self.holiday_years[0])
+
+    @patch("server.employee_management.apps.holidays.backends.pypi_holidays.country_holidays")
+    def test_get_holidays_no_holidays(self, mock_country_holidays):
+        mock_country_holidays.return_value = {}
+
+        backend = PyPiHolidaysBackend()
+
+        holidays = backend.get_holidays(
+            country="US", years=[2024], categories=("public",), language="en_US"
+        )
+
+        self.assertIsNone(holidays)
+
+    @patch("server.employee_management.apps.holidays.backends.pypi_holidays.country_holidays")
+    def test_get_holidays(self, mock_country_holidays):
+        mock_country_holidays.return_value = {
+            date(2024, 1, 1): "New Year's Day",
+            date(2024, 12, 25): "Christmas Day",
+        }
+
+        backend = PyPiHolidaysBackend()
+
+        holidays = backend.get_holidays(
+            country="US", years=[2024], categories=("public",), language="en_US"
+        )
+
+        expected_holidays = [
+            (date(2024, 1, 1), "New Year's Day"),
+            (date(2024, 12, 25), "Christmas Day"),
+        ]
+
+        self.assertEqual(holidays, expected_holidays)
 
 
 if __name__ == "__main__":
