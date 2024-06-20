2ff208f4e252e18bb79e11bbe340c50525b250b6
misrax <fordax05@gmail.com>
2024-06-20 04:52:33 +0300

    feat(employee-service): Adding EmployeeService to manage any busniess logic

---
+++ employee_management/apps/employee/services/employee_service.py
@@ -0,0 +1,20 @@
+import uuid
+
+from employee_management.apps.employee.models.employee import Employee
+from employee_management.apps.employee.repositories.employee_repository import (
+    EmployeeSQLiteRepository,
+)
+
+
+class EmployeeService:
+    def __init__(self, employee_repository: EmployeeSQLiteRepository = EmployeeSQLiteRepository):
+        self._employee_repository = employee_repository
+
+    def get_employee_by_id(self, employee_id: uuid.UUID) -> Employee | None:
+        return self._employee_repository.get(entity_id=employee_id)
+
+    def delete_employee_by_id(self, employee_id: uuid.UUID) -> None:
+        return self._employee_repository.delete(entity_id=employee_id)
+
+    def update_employee_by_id(self, employee_id: uuid.UUID, values: dict) -> Employee:
+        return self._employee_repository.update(entity_id=employee_id, update=values)
